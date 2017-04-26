3e938dbd6c4fbdb0450ce5097fffe7ad3536fc8c
Digital Alchemist <developer.anwar@gmail.com>
2017-04-25 22:54:20 -0400

    Blink BE, Service, Endpoints updated

--- blink/blink-endpoints/src/main/java/com/dc/blink/endpoints/EndpointBootManager.java
+++ blink/blink-endpoints/src/main/java/com/dc/blink/endpoints/EndpointBootManager.java
@@ -5,6 +5,8 @@ import static spark.Spark.get;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.dc.blink.endpoints.controller.BlinkController;
+
 /**
  * EndpointBootManager
  */
@@ -16,8 +18,9 @@ public class EndpointBootManager {
     public static void main( String[] args ){
     	LOGGER.info( "Hello World!" );
         
-        get("/hello", (req, res) -> "Hello World");
+        get("/hello", (req, res) -> "Hello World اردو" + "<br/ >" + "اُردو پوائنٹ کا باقاعدہ آغاز 14اگست 2000ء میں ہوا۔ اُردو پوائنٹ سے قبل یہ سائیٹ ’’اُردو کارڈز‘‘ کے نام سے مشہور تھی، جسکا آغاز 1998میں ہوا تھا۔ ’’اُردو کارڈز‘‘ پر پہلے اُردو میں کارڈز بھیجنے کی سہولت تھی، لیکن قارئین کی فرمائش پر اس میں شاعری کا اضافہ کیا گیا،جسکی بھرپور کامیابی اور شہرت کے بعد’’ اُردو پوائنٹ‘‘ کو تخلیق کیا گیا۔ آغاز میں اُردو پوائنٹ پر 12سیکشن تھے، اور یہ سادہ طریقے سے اپ ڈیٹ ہوتی تھی۔ اس میں کسی جدید ٹیکنالوجی کا استعمال نہیں کیا گیا تھا، سادہ HTMLصفحات کے ساتھ اس سائیٹ کوکئی سال چلایا جاتا رہا۔");
 
+        
         /**
          * URL : http://localhost:4567/greetings?username=DC
          */
@@ -30,9 +33,14 @@ public class EndpointBootManager {
          * URL : http://localhost:4567/greetings?username=DC
          * here :username is the path variable
          */
-        get("/:username", (req, res) -> { 
+        /*get("/:username", (req, res) -> { 
         	String name = req.params("username");
         	return "Username : "+name;
+        });*/
+        
+        
+        get("/blinks", (req, res) -> { 
+        	return new BlinkController().getAllBlinks();
         });
     }
 }
